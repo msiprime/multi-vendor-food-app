356dbd8b0d2ac00ce88f41f7442724a67da77e76
msiprime <msisakib958@gmail.com>
2024-08-29 22:43:15 +0600

    separated home and app with views, made it more professional

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:multi_vendor_food_app/main.dart';
+import 'package:multi_vendor_food_app/app/view/app_view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
